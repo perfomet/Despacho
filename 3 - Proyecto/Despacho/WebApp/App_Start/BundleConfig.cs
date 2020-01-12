e4f147c7d029d5d6234274ba136ddbcc0ea4b326
David Perdigón García <davidperdigon.g@gmail.com>
2020-01-12 16:43:23 -0300

    Creación pantalla de Login
    Se crea vista Login parcialmente

--- 3 - Proyecto/Despacho/WebApp/App_Start/BundleConfig.cs
+++ 3 - Proyecto/Despacho/WebApp/App_Start/BundleConfig.cs
@@ -9,22 +9,25 @@ namespace Despacho
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/js/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/js/jquery.validate*"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/js/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Content/js/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/login").Include(
+                      "~/Content/css/login.css"));
         }
     }
 }
