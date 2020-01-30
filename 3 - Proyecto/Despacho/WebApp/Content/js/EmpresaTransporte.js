9479d4d0be1327cc48e11b30ca069d6a6b5d6776
Paco <francisco.vivacuba@gmail.com>
2020-01-29 22:39:22 -0300

    Empresa Transporte

--- 3 - Proyecto/Despacho/WebApp/Content/js/EmpresaTransporte.js
+++ 3 - Proyecto/Despacho/WebApp/Content/js/EmpresaTransporte.js
@@ -5,34 +5,37 @@
   };
 
   let InitElementos = function () {
-    $.post("/EmpresaTransporte/Listar", { empresatransporteId: 0 }, function (empresasdetransportes) {
-      $("#listaEmpresaTransporte").mDatatable({
-        data: {
-          type: "local",
-          source: empresasdetransportes,
-          pageSize: 10
-        },
-        layout: {
-          theme: "default",
-          class: "",
-          scroll: !1,
-          footer: !1
-        },
-        sortable: !0,
-        pagination: !0,
-        search: {
-          input: $("#buscarEmpresaTransporte")
-        },
-        columns:
-          [
-            { field: "EmpresaTransporteId", title: "#", width: 50, selector: !1, textAlign: "center" },
-            { field: "Nombre", title: "Nombre", responsive: { visible: "lg" } },
-            { field: "EsPropia", title: "Es Propia", responsive: { visible: "lg" } },
-            { field: "EstaActivo", title: "Activo", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EstaActivo == true ? "Si" : "No"; } }
-          ], true, true);
-          });
+    if ($("#listaempresastransporte").length > 0) {
+      cargarTabla("EmpresaTransporteId", "EmpresaTransporte", { Id: 0 }, "#listaempresatransporte", "#buscarempresatransporte", [
+        { field: "EmpresaTransporteId", title: "#", width: 50, selector: !1, textAlign: "center" },
+        { field: "Nombre", title: "Nombre", responsive: { visible: "lg" } },
+        { field: "EsPropia", title: "Es Propia", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EsPropia == true ? "Si" : "No"; }  },
+        { field: "EstaActivo", title: "Activo", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EstaActivo == true ? "Si" : "No"; } }
+      ], true, true);
+    }
+
+    $('#btnGuardar').click(function () {
+      let id = $('#id').val();
+      let activo = $('#activo').val();
+      let nombre = $('#nombre').val();
+      let espropia = $('#espropia').val();
+      
+      $.post("/EmpresaTransporte/" + (id > 0 ? "Edit" : "Create"), {
+        EmpresaTransporteId: id,
+        Nombre: nombre,
+        EsPropia: espropia,
+        EstaActivo: activo
+      }, function (data) {
+        if (data.exito) {
+          mensaje("Exito", "Información guardada correctamente", "exito", function () { location.href = "/EmpresaTransporte/Index"; });
+        } else {
+          mensaje("Error", "No se pudo guardar la información", "error");
+        }
+      });
     });
   };
+
+
 
   return {
     init: function () {
