439961226cde77e610bb85608484be20a774def1
Paco <francisco.vivacuba@gmail.com>
2020-01-29 22:50:14 -0300

    Prioridad

--- 3 - Proyecto/Despacho/WebApp/Content/js/Prioridad.js
+++ 3 - Proyecto/Despacho/WebApp/Content/js/Prioridad.js
@@ -5,33 +5,33 @@
   };
 
   let InitElementos = function () {
-    $.post("/Prioridad/Listar", { id: 0 }, function (prioridades) {
-      $("#listaprioridades").mDatatable({
-        data: {
-          type: "local",
-          source: prioridades,
-          pageSize: 10
-        },
-        layout: {
-          theme: "default",
-          class: "",
-          scroll: !1,
-          footer: !1
-        },
-        sortable: !0,
-        pagination: !0,
-        search: {
-          input: $("#buscarprioridad")
-        },
-        columns: [
+    let InitElementos = function () {
+      if ($("#listaprioridades").length > 0) {
+        cargarTabla("PrioridadId", "Prioridad", { Id: 0 }, "#listaprioridades", "#buscarprioridad", [
           { field: "PrioridadId", title: "#", width: 50, selector: !1, textAlign: "center" },
-          { field: "Descripcion", title: "Descripción", responsive: { visible: "lg" } }
+          { field: "Descripcion", title: "Descripción", responsive: { visible: "lg" } },
+          { field: "EstaActivo", title: "Activo", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EstaActivo == true ? "Si" : "No"; } }
+        ], true, true);
+      }
 
-        ]
+      $('#btnGuardar').click(function () {
+        let id = $('#id').val();
+        let activo = $('#activo').val();
+        let descripcion = $('#descripcion').val();
+      
+        $.post("/Cliente/" + (id > 0 ? "Edit" : "Create"), {
+          PrioridadId: id,
+          Descripcion: descripcion,
+          EstaActivo: activo
+        }, function (data) {
+          if (data.exito) {
+            mensaje("Éxito", "Información guardada correctamente", "exito", function () { location.href = "/Prioridad/Index"; });
+          } else {
+            mensaje("Error", "No se pudo guardar la información", "error");
+          }
+        });
       });
-    });
-  };
-
+    };
   return {
     init: function () {
       Init();
