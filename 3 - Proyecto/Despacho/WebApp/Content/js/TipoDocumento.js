fd79babbf27d848f18ff2118b9ee0c52023e761b
Paco <francisco.vivacuba@gmail.com>
2020-01-29 22:17:24 -0300

    Otro más

--- 3 - Proyecto/Despacho/WebApp/Content/js/TipoDocumento.js
+++ 3 - Proyecto/Despacho/WebApp/Content/js/TipoDocumento.js
@@ -5,29 +5,30 @@
   };
 
   let InitElementos = function () {
-    $.post("/TipoDocumento/Listar", { id: 0 }, function (tiposdocumentos) {
-      $("#listatiposdocumentos").mDatatable({
-        data: {
-          type: "local",
-          source: tiposdocumentos,
-          pageSize: 10
-        },
-        layout: {
-          theme: "default",
-          class: "",
-          scroll: !1,
-          footer: !1
-        },
-        sortable: !0,
-        pagination: !0,
-        search: {
-          input: $("#buscartipodocumento")
-        },
-        columns:
-          [
-            { field: "TipoDocumentoId", title: "#", width: 50, selector: !1, textAlign: "center" },
-            { field: "Descripcion", title: "Descripción", responsive: { visible: "lg" } }
-          ]
+    if ($("#listaClientes").length > 0) {
+      cargarTabla("TipoDocumentoId", "TipoDocumento", { Id: 0 }, "#listatiposdedocumentos", "#buscartipodocumento",
+      [
+        { field: "TipoDocumentoId", title: "#", width: 50, selector: !1, textAlign: "center" },
+        { field: "Descripcion", title: "Descripción", responsive: { visible: "lg" } },
+        { field: "EstaActivo", title: "Activo", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EstaActivo == true ? "Si" : "No"; } }
+       ], true, true);
+    }
+    $('#btnGuardar').click(function () {
+      let id = $('#id').val();
+      let activo = $('#activo').val();
+      let descripcion = $('#descripcion').val();
+      
+
+      $.post("/TipoDocumento/" + (id > 0 ? "Edit" : "Create"), {
+       TipoDocumentoId: id,
+        Descripcion: descripcion,
+        EstaActivo: activo
+      }, function (data) {
+        if (data.exito) {
+          mensaje("Éxito", "Información guardada correctamente", "exito", function () { location.href = "/TipoDocumento/Index"; });
+        } else {
+          mensaje("Error", "No se pudo guardar la información", "error");
+        }
       });
     });
   };
