2effc7ff4bc610adb7b436e039e134f51a748977
Paco <francisco.vivacuba@gmail.com>
2020-01-29 22:23:52 -0300

    Gerencia

--- 3 - Proyecto/Despacho/WebApp/Content/js/Gerencia.js
+++ 3 - Proyecto/Despacho/WebApp/Content/js/Gerencia.js
@@ -4,33 +4,40 @@
     InitElementos();
   };
 
-  let InitElementos = function () {
-    $.post("/Gerencia/Listar", { Id: 0 }, function (gerencias) {
-      $("#listagerencias").mDatatable({
-        data: {
-          type: "local",
-          source: gerencias,
-          pageSize: 10
-        },
-        layout: {
-          theme: "default",
-          class: "",
-          scroll: !1,
-          footer: !1
-        },
-        sortable: !0,
-        pagination: !0,
-        search: {
-          input: $("#buscargerencia")
-        },
-        columns: [
+  let InitElementos = function ()
+    {
+    if ($("#listagerencia").length > 0)
+    {
+      cargarTabla("GerenciaId", "Descripcion", { Id: 0 }, "#listagerencias", "#buscargerencia",
+        [
           { field: "GerenciaId", title: "#", width: 50, selector: !1, textAlign: "center" },
           { field: "Descripcion", title: "Descripción", responsive: { visible: "lg" } },
-          { field: "ClienteId", title: "# Cliente", responsive: { visible: "lg" } }
-        ]
+          { field: "ClienteId", title: "# Cliente", responsive: { visible: "lg" } },
+          { field: "EstaActivo", title: "Activo", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EstaActivo == true ? "Si" : "No"; } }
+        ], true, true);
+
+    }
+    $('#btnGuardar').click(function () {
+      let id = $('#id').val();
+      let activo = $('#activo').val();
+      let descripcion = $('#descripcion').val();
+      let clienteid = $('#clienteid').val();
+      
+      $.post("/Gerencia/" + (id > 0 ? "Edit" : "Create"), {
+        GerenciaId: id,
+        Descripcion: descripcion,
+        ClienteId: clienteid,
+        EstaActivo: activo
+      }, function (data) {
+        if (data.exito) {
+          mensaje("Exito", "Información guardada correctamente", "exito", function () { location.href = "/Cliente/Index"; });
+        } else {
+          mensaje("Error", "No se pudo guardar la información", "error");
+        }
       });
     });
   };
+
 
   return {
     init: function () {
