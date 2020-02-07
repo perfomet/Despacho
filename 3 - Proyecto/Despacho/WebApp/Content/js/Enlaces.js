7b54ebea1e64a6ad66c952b20bc69047084e0100
Paco <francisco.vivacuba@gmail.com>
2020-02-07 14:43:51 -0300

    Mejoras a Enlaces

--- 3 - Proyecto/Despacho/WebApp/Content/js/Enlaces.js
+++ 3 - Proyecto/Despacho/WebApp/Content/js/Enlaces.js
@@ -10,13 +10,13 @@
     if ($("#listaenlaces").length > 0){
       cargarTabla("Enlaceid", "Enlaces", { Id: 0 }, "#listaenlaces", "#buscarenlace", [
       { field: "Enlaceid", title: "#", width: 50, selector: !1, textAlign: "center" },
-      { field: "RUT", title: "RUT", responsive: { visible: "lg" } },
-      { field: "DV", title: "DV", responsive: { visible: "lg" } },
-      { field: "Nombre", title: "Nombre", responsive: { visible: "lg" } },
-      { field: "PrimerApellido", title: "Primer Apellido", responsive: { visible: "lg" } },
+      { field: "RUT", title: "RUT", responsive: { visible: "lg" }},
+      { field: "DV", title: "DV", responsive: { visible: "lg" }},
+      { field: "Nombre", title: "Nombre", responsive: { visible: "lg" }},
+      { field: "PrimerApellido", title: "Primer Apellido", responsive: { visible: "lg" }},
       { field: "SegundoApellido", title: "Segundo Apellido", responsive: { visible: "lg" }},
       { field: "Email", title: "email", responsive: { visible: "lg" }},
-      { field: "TipoPersonal", title: "Tipo Personal", responsive: { visible: "lg" } },
+      { field: "TipoPersonal", title: "Tipo Personal", responsive: { visible: "lg" }},
       { field: "EstaActivo", title: "Activo", responsive: { visible: "lg" }, template: function (e, a, i) { return e.EstaActivo == true ? "Si" : "No"; } }
       ], true, true);
     }
@@ -34,13 +34,13 @@
 
       $.post("/Enlaces/" + (id > 0 ? "Edit" : "Create"), {
         Enlaceid: id,
-        rut: rut,
-        dv : dv,
-        nombre : nombre,
-        primerapellido : primerapellido,
-        segundoapellido : segundoapellido,
-        email : email,
-        tipopersonalid : tipopersonalid,
+        Rut: rut,
+        Dv : dv,
+        Nombre : nombre,
+        Primerapellido : primerapellido,
+        Segundoapellido : segundoapellido,
+        Email : email,
+        Tipopersonalid : tipopersonalid,
         EstaActivo: activo
 
       }, function (data) {
